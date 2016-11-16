60661bccf87be918ae14673a6e291fc51d1e61c0
chx <chxpostbox@outlook.com>
2016-11-16 19:33:13 +0800

    【Spring】Spring高级话题-Spring Aware

---
+++ springBoot/src/main/java/cn/hncu/p3/p1_SpringAware/AwareConfig.java
@@ -0,0 +1,16 @@
+package cn.hncu.p3.p1_SpringAware;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: 陈浩翔.
+ * Date: 2016/11/16.
+ * Time: 下午 6:48.
+ * Explain:配置类
+ */
+@Configuration
+@ComponentScan("cn.hncu.p3.p1_SpringAware")
+public class AwareConfig {
+}
