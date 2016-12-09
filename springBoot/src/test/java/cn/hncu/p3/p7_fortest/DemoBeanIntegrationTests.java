1187b15f9f4b82b74a98ab640cf13feaf3440c63
chx <chxpostbox@outlook.com>
2016-12-09 21:31:32 +0800

    spring--测试

---
+++ springBoot/src/test/java/cn/hncu/p3/p7_fortest/DemoBeanIntegrationTests.java
@@ -0,0 +1,13 @@
+package cn.hncu.p3.p7_fortest;
+
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: 陈浩翔.
+ * Date: 2016/12/9.
+ * Time: 下午 9:27.
+ * Explain:测试类---源码在src/test/java下
+ */
+public class DemoBeanIntegrationTests {
+}
